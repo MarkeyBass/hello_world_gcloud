d42f79bcb6d3f037e2277b356fb5a502ed5575ad
MarkeyBass <68155694+MarkeyBass@users.noreply.github.com>
2021-12-26 09:57:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START gae_python38_app]
-# [START gae_python3_app]
-
-# Setting virtual environment: $ python -m venv env
-#                              $ .\env\Scripts\activate
-
-# Following this tutorial
-# https://cloud.google.com/appengine/docs/standard/python3/quickstart#windows
-# Google Cloud project id: helloworld-335910
-# Google Cloud project number: 578935054569
-
 # THE LINK TO THE RUNNING APP:  https://helloworld-335910.ew.r.appspot.com/
 
 from flask import Flask
